07cc45b3bf131314c6f6e3e6372d729516dd6e58
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-11-12 14:28:12 -0600

    Additional theme config

--- apps/storefront/src/theme-config.ts
+++ apps/storefront/src/theme-config.ts
@@ -2,13 +2,26 @@ import { Theme, ThemeOptions } from "@mui/material"
 
 export const getThemeConfig = (theme: Theme) => {
   return {
-    // TODO: Use `palette` to set the secondary color
-    
+    palette: {
+      secondary: {
+        main: '#f2a579',
+      },
+    },
     typography: {
       fontFamily: 'Montserrat, Arial, sans-serif',
-
-      // TODO: Apply other typography config
-      //  - Apply a different font size for body text, h3 headings, and buttons
+      body1: {
+        fontSize: '1.25rem',
+      },
+      body2: {
+        fontSize: '1rem',
+      },
+      h3: {
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+      },
+      button: {
+        fontSize: '1rem',
+      },
     },
   } as ThemeOptions;
 }
