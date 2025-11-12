de26d6d28fdc9ce61129d1ef0fc8ec75ae96f956
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-11-12 14:27:45 -0600

    Additional theme config - COMMENTS

--- apps/storefront/src/theme-config.ts
+++ apps/storefront/src/theme-config.ts
@@ -2,8 +2,13 @@ import { Theme, ThemeOptions } from "@mui/material"
 
 export const getThemeConfig = (theme: Theme) => {
   return {
+    // TODO: Use `palette` to set the secondary color
+    
     typography: {
       fontFamily: 'Montserrat, Arial, sans-serif',
+
+      // TODO: Apply other typography config
+      //  - Apply a different font size for body text, h3 headings, and buttons
     },
   } as ThemeOptions;
 }
