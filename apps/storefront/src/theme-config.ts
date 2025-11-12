62043c47cc710e2ce97ff9351312aec781f0c921
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-11-12 14:27:45 -0600

    TODO: Additional theme config

--- apps/storefront/src/theme-config.ts
+++ apps/storefront/src/theme-config.ts
@@ -9,9 +9,13 @@ export const getThemeConfig = (theme: Theme) => {
       primary: {
         main: theme.palette.primary.main,
       },
+      // TODO: Set the secondary color
     },
     typography: {
       fontFamily: 'Montserrat, Arial, sans-serif',
+
+      // TODO: Apply other typography config
+      //  - Apply a different font size for body text, h3 headings, and buttons
     },
   } as ThemeOptions;
 }
