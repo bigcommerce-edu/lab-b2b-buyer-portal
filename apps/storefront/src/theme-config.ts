20c3a6fc6dd298db154cc78e708ec0f988295966
Chris Nanninga <chris.nanninga@bigcommerce.com>
2026-02-03 11:56:53 -0600

    Add custom Material UI theme configuration

--- apps/storefront/src/theme-config.ts
+++ apps/storefront/src/theme-config.ts
@@ -1,7 +1,17 @@
 import { Theme, ThemeOptions } from "@mui/material"
 
 export const getThemeConfig = (theme: Theme) => {
-  // TODO: Return a theme options object
-  //  - Apply a custom font with `typography.fontFamily`
-  throw new Error('getThemeConfig not implemented');
+  return {
+    palette: {
+      background: {
+        default: theme.palette.background.default,
+      },
+      primary: {
+        main: theme.palette.primary.main,
+      },
+    },
+    typography: {
+      fontFamily: 'Montserrat, Arial, sans-serif',
+    },
+  } as ThemeOptions;
 }
