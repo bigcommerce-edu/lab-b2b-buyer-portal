080907c0b740f3f307e8230eee8a8bb19ba9e73e
Chris Nanninga <chris.nanninga@bigcommerce.com>
2026-02-12 13:50:00 -0600

    Add sections for other recent records on Overview page

--- apps/storefront/src/pages/Overview/index.tsx
+++ apps/storefront/src/pages/Overview/index.tsx
@@ -14,6 +14,9 @@ import { type SetOpenPage } from '@/pages/SetOpenPage';
 
 import Identity from "./components/Identity";
 import RecentOrders from "./components/RecentOrders";
+import RecentInvoices from "./components/RecentInvoices";
+import RecentShoppingLists from "./components/RecentShoppingLists";
+import RecentQuotes from "./components/RecentQuotes";
 
 import { useB3Lang } from "@/lib/lang";
 
@@ -27,8 +30,9 @@ export default function Overview({
   const b3Lang = useB3Lang();
 
   const [ordersOpen, setOrdersOpen] = useState<boolean>(false);
-
-  // TODO: Create `*Open` state values for invoices, shopping lists, and quotes
+  const [invoicesOpen, setInvoicesOpen] = useState<boolean>(false);
+  const [shoppingListsOpen, setShoppingListsOpen] = useState<boolean>(false);
+  const [quotesOpen, setQuotesOpen] = useState<boolean>(false);
 
   const allowOrders = validatePermissionWithComparisonType({
     code: newPermissions.ordersPermissionCodes,
@@ -36,7 +40,23 @@ export default function Overview({
     containOrEqual: 'contain',
   });
 
-  // TODO: Create `allow*` variables for invoices, shopping lists, and quotes
+  const allowInvoices = validatePermissionWithComparisonType({
+    code: newPermissions.invoicePermissionCodes,
+    level: permissionLevels.COMPANY,
+    containOrEqual: 'contain',
+  });
+
+  const allowShoppingLists = validatePermissionWithComparisonType({
+    code: newPermissions.shoppingListsPermissionCodes,
+    level: permissionLevels.USER,
+    containOrEqual: 'contain',
+  });
+
+  const allowQuotes = validatePermissionWithComparisonType({
+    code: newPermissions.quotesPermissionCodes,
+    level: permissionLevels.COMPANY,
+    containOrEqual: 'contain',
+  });
 
   return (
     <>
@@ -77,7 +97,77 @@ export default function Overview({
         </Grid>
         )}
 
-        {/* TODO: Conditionally output invoices, shopping lists, and quotes */}
+        {allowInvoices && (
+        <Grid
+          item
+          key="recent-invoices"
+          xs={12}
+        >
+          <Accordion
+            onChange={(_e, isExpanded) => setInvoicesOpen(isExpanded)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+            >
+              <Typography variant="h3">{b3Lang('overview.recentInvoices')}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <RecentInvoices
+                startLoad={invoicesOpen}
+                setOpenPage={setOpenPage}
+              />
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+        )}
+
+        {allowShoppingLists && (
+        <Grid
+          item
+          key="recent-shopping-lists"
+          xs={12}
+        >
+          <Accordion
+            onChange={(_e, isExpanded) => setShoppingListsOpen(isExpanded)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+            >
+              <Typography variant="h3">{b3Lang('overview.recentShoppingLists')}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <RecentShoppingLists
+                startLoad={shoppingListsOpen}
+                setOpenPage={setOpenPage}
+              />
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+        )}
+
+        {allowQuotes && (
+        <Grid
+          item
+          key="recent-quotes"
+          xs={12}
+        >
+          <Accordion
+            onChange={(_e, isExpanded) => setQuotesOpen(isExpanded)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+            >
+              <Typography variant="h3">{b3Lang('overview.recentQuotes')}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <RecentQuotes
+                startLoad={quotesOpen}
+                setOpenPage={setOpenPage}
+              />
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+        )}
       </Grid>
     </>
   );
